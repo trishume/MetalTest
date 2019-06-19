1ca00c8cb10e1bc03284a8d40bfdb5ff6c75021f
Tristan Hume <tris.hume@gmail.com>
2019-06-19 17:53:06 -0400

    Basic MTKView triangle

--- MetalTest2/AppDelegate.swift
+++ MetalTest2/AppDelegate.swift
@@ -1,26 +1,92 @@
-//
-//  AppDelegate.swift
-//  MetalTest2
-//
-//  Created by Tristan Hume on 2019-06-19.
-//  Copyright © 2019 Tristan Hume. All rights reserved.
-//
-
 import Cocoa
+import MetalKit
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+class MyApplication: NSApplication {
+    let strongDelegate = AppDelegate()
+    
+    override init() {
+        super.init()
+        self.delegate = strongDelegate
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
-
 }
 
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, MTKViewDelegate {
+    var window: NSWindow!
+    var metalView: MTKView!
+    let device = MTLCreateSystemDefaultDevice()!
+    var commandQueue: MTLCommandQueue!
+    var pipelineState: MTLRenderPipelineState!
+    
+    var viewportSize: simd_uint2 = vector2(0, 0)
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window = NSWindow(contentRect: NSMakeRect(10, 10, 600, 600), styleMask: [.titled, .resizable, .closable, .miniaturizable], backing: .buffered, defer: false)
+        window.makeKeyAndOrderFront(nil)
+        window.delegate = self
+        
+        metalView = MTKView(frame: NSRect(origin: CGPoint.zero, size: window.frame.size), device: device)
+        metalView.delegate = self
+        
+        metalView.isPaused = true
+        metalView.enableSetNeedsDisplay = true
+        metalView.needsDisplay = true
+//        metalView.presentsWithTransaction = true
+        
+        window.contentView = metalView
+        commandQueue = device.makeCommandQueue()
+        do {
+            let library = device.makeDefaultLibrary()!
+            let pipelineDescriptor = MTLRenderPipelineDescriptor()
+            pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+            pipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_main")
+            pipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_main")
+            pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch {
+            print("Failed to create pipeline")
+        }
+        
+        metalView.draw()
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        viewportSize.x = UInt32(size.width)
+        viewportSize.y = UInt32(size.height)
+        print("changed \(viewportSize)")
+//        view.draw()
+        metalView.needsDisplay = true
+    }
+    
+    func draw(in view: MTKView) {
+        guard let _commandQueue = commandQueue else { return }
+        guard let commandBuffer = _commandQueue.makeCommandBuffer() else { return }
+        guard let passDescriptor = view.currentRenderPassDescriptor else { return }
+        guard let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: passDescriptor) else { return }
+        let vertexData: [Float] = [ 250, -250, 0, 1, 0, 0,
+                                    -250, -250, 0, 0, 1, 0,
+                                    0,  250, 0, 0, 0, 1 ]
+        encoder.setVertexBytes(vertexData,
+                               length: vertexData.count * MemoryLayout<Float>.stride,
+                               index: 0)
+        encoder.setVertexBytes(&viewportSize,
+                               length: MemoryLayout<simd_uint2>.stride,
+                               index: 1)
+        encoder.setRenderPipelineState(pipelineState)
+        encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
+        encoder.endEncoding()
+        commandBuffer.present(view.currentDrawable!)
+        commandBuffer.commit()
+        print("draw \(viewportSize)")
+//        commandBuffer.waitUntilScheduled()
+//        view.currentDrawable!.present()
+//        commandBuffer.waitUntilCompleted()
+    }
+}
---
+++ MetalTest2/Shaders.metal
@@ -0,0 +1,44 @@
+//
+//  Shaders.metal
+//  MetalTest2
+//
+//  Created by Tristan Hume on 2019-06-19.
+//  Copyright © 2019 Tristan Hume. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+struct VertexIn {
+    packed_float3 position;
+    packed_float3 color;
+};
+struct VertexOut {
+    float4 position [[position]];
+    float4 color;
+};
+vertex VertexOut vertex_main(device const VertexIn *vertices [[buffer(0)]],
+                             constant vector_uint2 *viewportSizePointer [[buffer(1)]],
+                             uint vertexID [[vertex_id]]) {
+    VertexOut out;
+    // Index into the array of positions to get the current vertex.
+    // The positions are specified in pixel dimensions (i.e. a value of 100
+    // is 100 pixels from the origin).
+    float2 pixelSpacePosition = vertices[vertexID].position.xy;
+    
+    // Get the viewport size and cast to float.
+    vector_float2 viewportSize = vector_float2(*viewportSizePointer);
+    pixelSpacePosition -= (viewportSize / 2.0) - 300.0;
+    
+    
+    // To convert from positions in pixel space to positions in clip-space,
+    //  divide the pixel coordinates by half the size of the viewport.
+    out.position = vector_float4(0.0, 0.0, 0.0, 1.0);
+    out.position.xy = pixelSpacePosition / (viewportSize / 2.0);
+    
+    // Pass the input color directly to the rasterizer.
+    out.color = float4(vertices[vertexID].color, 1.0);
+    return out;
+}
+fragment float4 fragment_main(VertexOut in [[stage_in]]) {
+    return in.color;
+    }
