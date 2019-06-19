6f0c490cd4f67498a964fd0018a05c3e61786d91
Tristan Hume <tris.hume@gmail.com>
2019-06-19 16:32:52 -0400

    Initial Commit

---
+++ MetalTest2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalTest2
+//
+//  Created by Tristan Hume on 2019-06-19.
+//  Copyright © 2019 Tristan Hume. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
